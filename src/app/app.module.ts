d7b386de700df9c99a30a1895caf0e70e0a98bb9
Danica Gazdić <danicagazdic@gmail.com>
2022-11-28 23:50:23 +0100

    Initial design of toolbar - in need of color changegit add .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './components/navigationBar/navigation-bar/navigation-bar.component';
+import { MaterialModule } from 'src/infrastructure/material.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavigationBarComponent } from './components/navigationBar/navigation-ba
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
