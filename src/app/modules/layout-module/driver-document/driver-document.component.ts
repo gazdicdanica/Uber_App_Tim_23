5656c319fa119c0340ade62dc35f46008c6ad101
Danica Gazdić <danicagazdic@gmail.com>
2023-01-19 23:35:41 +0100

    Implemented upload of new document

--- src/app/modules/layout-module/driver-document/driver-document.component.ts
+++ src/app/modules/layout-module/driver-document/driver-document.component.ts
@@ -1,5 +1,7 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Document } from '../../model/Document';
+import { DriverService } from '../../services/driver/driver.service';
 
 @Component({
   selector: 'app-driver-document',
@@ -10,16 +12,60 @@ export class DriverDocumentComponent implements OnInit{
 
   @Input() input!: string;
 
+  base64: string = "";
+  upload : boolean = false;
+
+  constructor(private driverService: DriverService){}
+
   documentForm = new FormGroup({
     fileInput: new FormControl('')
   });
 
   ngOnInit(): void {
-    console.log(this.input);
+    
+  }
+
+  ngAfterViewInit(){
+    this.driverService.getDocuments().subscribe({
+      next: (result) => {
+        for(let x of result){
+          console.log(x.name);
+          if (x.name == this.input){
+            this.base64 =  "data:image/png;base64," + x.documentImage;
+            this.upload = true;
+            break;
+          }
+        }
+      }
+    })
+  }
+
+  previewImage(input : any) : void{
+    let file: File = input.files[0];
+    let reader: FileReader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = (e: any) => {
+      this.base64 = e.target.result.replace(/(\r\n|\n|\r)/gm, "");
+      this.upload = true;
+      
+    }
+  }
+
+  imageListener($event: any): void{
+    this.previewImage($event.target);
+    // this.updateDocument();
   }
 
   updateDocument() : void{
-    
+    const pic = this.documentForm.value.fileInput;
+    let document : Document = new Document(this.input, this.base64.split(",")[1], 0);
+    this.driverService.addDocument(document).subscribe({
+      next : (res) => {
+        console.log(res);
+        
+      }
+    });
   }
 
 }
--- src/app/modules/services/driver/driver.service.ts
+++ src/app/modules/services/driver/driver.service.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/enviroments/environment';
 import { Location } from '../../map/Location';
 import { Observable } from 'rxjs';
 import { AuthService } from '../../auth/auth.service';
+import { Document } from '../../model/Document';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,15 @@ export class DriverService {
 
   getId(): number {
     return this.authService.getId();
+  }
+
+  addDocument(document : Document) : Observable<Document>{
+    let id : number = this.getId();
+    document.driverId = id;
+    return this.httpClient.post<Document>(environment.apiHost + "/driver/" + this.getId() + "/documents", document);
+  }
+
+  getDocuments(){
+    return this.httpClient.get<Document[]>(environment.apiHost+"/driver/" + this.getId() + "/documents");
   }
 }
