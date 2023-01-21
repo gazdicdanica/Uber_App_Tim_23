bc0804440d4dadfe18667c0b00dad8e5906e8660
Danica Gazdić <danicagazdic@gmail.com>
2023-01-21 17:13:32 +0100

    Code for websockets from example

--- src/app/modules/model/vehicle.ts
+++ src/app/modules/model/vehicle.ts
@@ -1,17 +1,23 @@
+import { Location } from "./Location";
+
 export class Vehicle{
+    id: number;
     model: string;
     vehicleType: string;
     licenseNumber: string;
     passengerSeats: number;
     babyTransport: boolean;
     petTransport: boolean;
+    currentLocation: Location;
 
-    constructor(model: string, type: string, license: string, capacity: number, babies: boolean, pets: boolean){
+    constructor(id: number, model: string, type: string, license: string, capacity: number, babies: boolean, pets: boolean, loc : Location){
         this.model = model;
         this.vehicleType = type;
         this.licenseNumber = license;
         this.passengerSeats = capacity;
         this.babyTransport = babies;
         this.petTransport = pets;
+        this.id = id;
+        this.currentLocation = loc;
     }
 }
